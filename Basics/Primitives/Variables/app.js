5fcb5905bf1b3e13abd29b1a3b56b7e9d0644a05
Clarenceintech <clarenceigwebuike@gmail.com>
2025-01-21 02:22:46 +0000

    First Commit

---
+++ Basics/Primitives/Variables/app.js
@@ -0,0 +1,13 @@
+// console.warn("This is a warning!")
+// console.table({name: "Clarence", age: 22});
+// console.log();
+
+// 1. Create variable name (name) & store your name.
+// 2. Create variable name (whatDoYouWannaBecomeInYourLife) & store "programmer"
+// 3. Create variable name (gender) & store your gender.
+// 4. Create variable name (twitterHandle) & store your twitter handle.
+// 5. Finally log all variables to the console.
+
+let name = "Clarence";
+let whatDoYouWannaBecomeInYourLife = "programmer";
+let gender = "male";
