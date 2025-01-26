9045970aec1130240ae86d3c36b5c3a09dba1523
Clarenceintech <clarenceigwebuike@gmail.com>
2025-01-26 02:17:17 +0000

    Omo

--- Control Flow/control.js
+++ Control Flow/control.js
@@ -18,21 +18,53 @@
 
 // }
 
-let size = 8;
-let board = '';
+// let size = 8;
+// let board = '';
 
-for (let y= 0; y < size; y++) {
-    for(let x= 0; x < size; x++) {
-        if ((x+y) % 2 == 0) {
-            board += " "
+// for (let y= 0; y < size; y++) {
+//     for(let x= 0; x < size; x++) {
+//         if ((x+y) % 2 == 0) {
+//             board += " "
 
-        } else {
-            board += "#";
-        }
-    }
+//         } else {
+//             board += "#";
+//         }
+//     }
 
-    // board += "\n"
-}
+//     // board += "\n"
+// }
 
-console.log(board);
+// console.log(board);
 
+
+// Print your name & numbers from 0 through 1000
+
+// let name = "Clarence";
+// for ( counter = 0; counter < 1000; counter++ ) {
+//     console.log(counter + name);
+    
+// }
+
+// Print "Your Name" 100 times to the console using while loop
+// Iteration will start from 10
+
+// let myName = "Clarence";
+// let counter = 10;
+
+// while(counter <= 100) {
+//     console.log(myName);
+//     counter++;
+    
+// }
+
+
+// Print "Your Name" 400 times to the console using do-while-loop.
+// Iteration should start from 20.
+
+let counter = 20;
+
+do {
+    console.log("Clarence", counter);
+    counter ++;
+    
+} while (counter <= 400);
--- Functions/functions.js
+++ Functions/functions.js
@@ -1,12 +1,54 @@
-const hummus = function(factor) {
-    const ingredient = function(amount, unit, name){
-        let ingredientAmount = amount * factor;
-        if (ingredientAmount > 1) {
-            unit +="s";
+// const hummus = function(factor) {
+//     const ingredient = function(amount, unit, name){
+//         let ingredientAmount = amount * factor;
+//         if (ingredientAmount > 1) {
+//             unit +="s";
+//         }
+//     }
+
+//     console.log(`${ingredientAmount} ${unit} ${name}`);
+    
+// }
+// function findSolution(target) {
+//     // Helper function that recursively explores possible operations
+//     function find(current, history) {
+//         // Base case: if the current number equals the target, return the history
+//         if (current == target) {
+//             return history;
+//         }
+//         // Base case: if the current number exceeds the target, stop exploring this path
+//         else if (current > target) {
+//             return null;
+//         }
+//         // Recursive case: try adding 5 or multiplying by 3
+//         else {
+//             // Use null coalescing to return the first valid solution
+//             return find(current + 5, `(${history} + 5)`) ?? find(current * 3, `(${history} * 3)`);
+//         }
+//     }
+//     // Start the recursion with the initial number 1 and its history
+//     return find(1, "1");
+// }
+
+console.log(findSolution(11)); // Output: ((1 + 5) + 5)
+
+function findSolution(target) {
+    function find(current, history) {
+        if (current == target) {
+            return history;
+
+        }
+        else if (current > target) {
+            return null;
+        }
+        else {
+            return find(current + 5, `(${history} + 5)`) ?? find(current*3, `(${history} * 3)`);
+
         }
     }
 
-    console.log(`${ingredientAmount} ${unit} ${name}`);
-    
+    return find(1, "1");
 }
+
+console.log(findSolution(27));
 
